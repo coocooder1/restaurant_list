85bdbe6bea50c33203c8f09d2c3c5dee5fea6ed9
coocooder1 <121811676+coocooder1@users.noreply.github.com>
2023-04-11 19:52:59 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
-// app.js
-// require packages used in the project
-const express = require('express')
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const express = require("express")
+const exphbs = require("express-handlebars")
+const methodOverride = require("method-override")
+const routes = require("./routes")
+const bodyParser = require('body-parser')// 引用 body-parser
+require("./config/mongoose")
+
+
+
 const app = express()
 const port = 3000
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
 
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(methodOverride("_method"))
+app.use(routes)
 
-
-
-
-// setting static files
-app.use(express.static('public'))
-
-// routes setting
-app.get('/', (req, res) => {
-
-  res.render('index', { restaurants: restaurantList.results })
-})
-
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => req.params.restaurant_id === restaurant.id.toString())
-  res.render('show', { restaurant: restaurant })
-})
-
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-  res.render('index', { restaurants: restaurants, keyword: keyword })
-})
-
-
-
-
-
-// start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
+  console.log(`Listening on http://localhost:${port}`)
 })
