2ac8d210beaa478d3715c8629d4ef4a2e32e0a60
coocooder1 <121811676+coocooder1@users.noreply.github.com>
2023-04-14 21:20:18 +0800

    upgrade restaurants

---
+++ routes/modules/restaurants.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const router = express.Router()
+const Restaurant = require("../../models/Restaurant")
+
+// New Page
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurantsData => res.render("show", { restaurantsData }))
+    .catch(error => console.log(error))
+})
+
+
+//Add New
+router.post("/", (req, res) => {
+  return Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+//Detail 
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurantsData => res.render("show", { restaurantsData }))
+    .catch(error => console.log(error))
+})
+
+//Edit
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurantsData => res.render("edit", { restaurantsData }))
+    .catch(error => console.log(error))
+})
+
+
+// Update(PUT)
+router.put("/:id", (req, res) => {
+  const id = req.params.id
+  const editData = req.body
+  return Restaurant.findById(id)
+    .then(restaurantsData => {
+      restaurantsData.set(editData)
+      return restaurantsData.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:number', async (req, res) => {
+  try {
+    const _id = req.params.number
+    await Restaurant.findByIdAndDelete({ _id })
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+module.exports = router
